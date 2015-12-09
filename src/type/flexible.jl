9ab9e48678f8b1f3bd63cbfdd6c07ab0cb18e172
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-09 02:02:49 -0500

    Create flexible.jl

---
+++ src/type/flexible.jl
@@ -0,0 +1,68 @@
+#=  the Type realization underlying all FlexFloat computation  =#
+
+typealias Clay AbstractFloat # was Union{Float64,Float32}, other types can be added
+
+abstract EnhancedFloat <: Real
+
+#=
+          Sculpts are as intervals,
+          bounded about lo and hi, covering amidst lo and hi.
+            Each boundry either is Closed(Cl) or it is Open(Op).
+            e.g. ClOp(lo,hi) has a closed lower bound, and a open upper bound.
+=#
+abstract Sculpt <: EnhancedFloat
+
+type ClCl <: Sculpt end
+type ClOp <: Sculpt end
+type OpCl <: Sculpt end
+type OpOp <: Sculpt end
+
+abstract Supple{S,C} <: EnhancedFloat  # EnhancedFloat pulls in Real
+         #        C parameterizes the internal arithmetic type at work
+         #      S paramterizes boundedness as OpOp, ClOp, OpCl or ClCl
+
+immutable Flex{S<:Sculpt, C<:Clay} <: Supple{S,C}
+    lo::C
+    hi::C
+end
+
+@inline Flex{S<:Sculpt, C<:Clay}(::Type{S}, lo::C, hi::C) = Flex{S,C}(lo,hi)
+@inline Flex{S<:Sculpt, C<:Clay}(::Type{S}, x::C) = Flex{S,C}(x,x)
+@inline function FlexLoHi{S<:Sculpt, C<:Clay}(::Type{S}, lo::C, hi::C)
+    lo,hi = minmax(lo,hi)
+    Flex{S,C}(lo,hi)
+end
+
+ClCl{C<:Clay}(lo::C,hi::C) = Flex(ClCl, lo, hi)
+ClOp{C<:Clay}(lo::C,hi::C) = Flex(ClOp, lo, hi)
+OpCl{C<:Clay}(lo::C,hi::C) = Flex(OpCl, lo, hi)
+OpOp{C<:Clay}(lo::C,hi::C) = Flex(OpOp, lo, hi)
+
+ClCl{C<:Clay}(x::C) = Flex(ClCl, x)
+ClOp{C<:Clay}(x::C) = Flex(ClOp, x)
+OpCl{C<:Clay}(x::C) = Flex(OpCl, x)
+OpOp{C<:Clay}(x::C) = Flex(OpOp, x)
+
+# CC,OC,CO,OO are lo<=hi enforcing versions of ClCl,ClOp,OpCl,OpOp
+CC{C<:Clay}(x::C) = Flex(ClCl, x)
+CO{C<:Clay}(x::C) = Flex(ClOp, x)
+OC{C<:Clay}(x::C) = Flex(OpCl, x)
+OO{C<:Clay}(x::C) = Flex(OpOp, x)
+
+CC{C<:Clay}(lo::C,hi::C) = FlexLoHi(ClCl, lo, hi)
+function CO{C<:Clay}(lo::C,hi::C) 
+   if lo>hi
+      Flex(OpCl, hi, lo)
+   else
+      Flex(ClOp, hi, lo)
+   end
+end   
+function OC{C<:Clay}(lo::C,hi::C) 
+   if lo>hi
+      Flex(ClOp, hi, lo)
+   else
+      Flex(OpCl, hi, lo)
+   end
+end   
+OO{C<:Clay}(lo::C,hi::C) = FlexLoHi(OpOp, lo, hi)
+
