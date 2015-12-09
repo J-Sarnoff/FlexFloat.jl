a26829b729660b2605a1b806967481f67152ad6a
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-09 05:22:56 -0500

    Update primitive.jl

--- src/type/primitive.jl
+++ src/type/primitive.jl
@@ -5,21 +5,21 @@ value{S<:Sculpt, C<:Clay}(x::Flex{S,C})  = (lo,hi)
 
 # each boundry is closed(false) or Open (true)
 # get boundries from Sculpt
-boundries(::Type{ClCl}) = (false,false)
-boundries(::Type{ClOp}) = (false,true)
-boundries(::Type{OpCl}) = (true,false)
-boundries(::Type{OpOp}) = (true,true)
+boundries(::Type{CLCL}) = (false,false)
+boundries(::Type{CLOP}) = (false,true)
+boundries(::Type{OPCL}) = (true,false)
+boundries(::Type{OPOP}) = (true,true)
 # get Sculpt from boundries
 boundries{S<:Sculpt, C<:Clay}(x::Flex{S,C}) = boundries(S)
 # get Sculpt from boundries
-const Boundries = [ClCl, OpCl, ClOp, OpOp]
+const Boundries = [CLCL, OPCL, CLOP, OPOP]
 boundries{B<:Bool}(lo::B,hi::B) = Boundries[ one(Int8)+reinterpret(Int8,lo)+(reinterpret(Int8,hi)<<1) ]
 
 # select type corresponding to negation of type given
-@inline negate(::Type{ClCl}) = ClCl
-@inline negate(::Type{ClOp}) = OpCl
-@inline negate(::Type{OpCl}) = ClOp
-@inline negate(::Type{OpOp}) = OpOp
+@inline negate(::Type{CLCL}) = CLCL
+@inline negate(::Type{CLOP}) = OPCL
+@inline negate(::Type{OPCL}) = CLOP
+@inline negate(::Type{OPOP}) = OPOP
 
 #=
     Closed bounds are reached but Not Exceeded.
