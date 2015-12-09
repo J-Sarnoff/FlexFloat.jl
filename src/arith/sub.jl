9e0d2cca0547281f7bb8d8b77b69ca955a23e160
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-09 05:32:57 -0500

    Update sub.jl

--- src/arith/sub.jl
+++ src/arith/sub.jl
@@ -22,3 +22,9 @@ function (-){S<:Sculpt, T<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{T,C})
        
     cType(lo,hi)
 end
+
+(-){S<:Sculpt,C<:Clay}(a::Flex{S,C}, b::C) = (-)(a, Flex{S,C}(b))
+(-){S<:Sculpt,C<:Clay}(a::C, b::Flex{S,C}) = (-)(Flex{S,C}(a), b)
+
+(-){S<:Sculpt,C<:Clay,T<:Union{AbstractFloat,Integer}}(a::Flex{S,C}, b::T) = (-)(a, Flex{S,C}(convert(C,b)))
+(-){S<:Sculpt,C<:Clay,T<:Union{AbstractFloat,Integer}}(a::T, b::Flex{S,C}) = (-)(Flex{S,C}(convert(C,a)), b)
