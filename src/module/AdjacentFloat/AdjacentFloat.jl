d6f4e6c380bf7b18be7c9aff95a5e87aba904588
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-09 02:07:06 -0500

    Update AdjacentFloat.jl

--- src/module/AdjacentFloat/AdjacentFloat.jl
+++ src/module/AdjacentFloat/AdjacentFloat.jl
@@ -3,18 +3,7 @@ module AdjacentFloat
 export prevFloat, nextFloat, prevprevFloat, nextnextFloat,
        nextAwayFromZero, nextnextAwayFromZero,
        nextNearerToZero, nextnextNearerToZero
-#=
-  These routines return +/-Inf when given +/-Inf.
-  That differs from nextfloat(-Inf) == -realmax(), prevfloat(Inf) == realmax()
-  (prevfloat(Inf)==Inf makes more sense to me, and likely is more helpful).
-  And steps twice when given values of very small magnitude (see paper).
-  The alternative implementation, converting to [U]Int and adding/subtracting 1,
-  returns NaN when given +/-Inf -- and checking for Inf adds branching. 
-  ref:
-  'Predecessor and Successor In Rounding To Nearest' by Siegried M. Rump
-  "The routines deliver the exact answer except for a small range near underflow,
-   in which case the true result is overestimated by eta [the value added/subtracted below]."
-=#
+
 # exact for |x| > 8.900295434028806e-308
 nextNearerToZero(x::Float64)   = (x-1.1102230246251568e-16*x)-5.0e-324  # do not simplify
 nextAwayFromZero(x::Float64)   = (x+1.1102230246251568e-16*x)+5.0e-324  # do not simplify
