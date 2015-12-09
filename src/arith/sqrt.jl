2afdc258c86ed91792712fc3de1b0f4aca5d96a2
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-09 06:09:04 -0500

    Update sqrt.jl

--- src/arith/sqrt.jl
+++ src/arith/sqrt.jl
@@ -1,1 +1,9 @@
-
+for T in (:CLCL, :CLOP, :OPCL, :OPOP)
+   @eval begin
+       function (sqrt){C<:Clay}(a::Flex{$T,C})
+           lo = (sqrt)(a.lo, b.lo, RoundDown)
+           hi = (sqrt)(a.hi, b.hi, RoundUp)
+           Flex{$T,C}(lo,hi)
+       end
+   end       
+end
