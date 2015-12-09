011b94bb814b3b9517ac48ad9bf6b5cd8032e990
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-09 08:03:33 -0500

    Update valueops.jl

--- src/arith/valueops.jl
+++ src/arith/valueops.jl
@@ -18,3 +18,27 @@ ldexp{S<:Sculpt,C<:Clay,I<:Union{Int32,Int64}}(::Type{S},a::Tuple{Tuple{C,I},Tup
    (S)(ldexp(a[1][1],a[1][2]),ldexp(a[2][1],a[2][2]))
 ldexp{C<:Clay,I<:Union{Int32,Int64}}(a::Tuple{Tuple{C,I},Tuple{C,I}}) = 
    (ClCl)(ldexp(a[1][1],a[1][2]),ldexp(a[2][1],a[2][2]))
+   
+#= from Julia
+`decompose(x)`: non-canonical decomposition of rational values as `num*2^pow/den`.
+The decompose function is the point where rational-valued numeric types that support
+hashing hook into the hashing protocol. `decompose(x)` should return three integer
+values `num, pow, den`, such that the value of `x` is mathematically equal to
+    num*2^pow/den
+The decomposition need not be canonical in the sense that it just needs to be *some*
+way to express `x` in this form, not any particular way – with the restriction that
+`num` and `den` may not share any odd common factors. They may, however, have powers
+of two in common – the generic hashing code will normalize those as necessary.
+Special values:
+ - `x` is zero: `num` should be zero and `den` should have the same sign as `x`
+ - `x` is infinite: `den` should be zero and `num` should have the same sign as `x`
+ - `x` is not a number: `num` and `den` should both be zero
+=#
+
+decompose{S<:Sculpt, C<:Clay}(x::Flex{S,C}) = decompose( mid(x) )
+
+isnan{S<:Sculpt, C<:Clay}(x::Flex{S,C}) = (isnan(x.lo) | isnan(x.hi))
+isinf{S<:Sculpt, C<:Clay}(x::Flex{S,C}) = (isinf(x.lo) | isinf(x.hi))
+isfinite{S<:Sculpt, C<:Clay}(x::Flex{S,C}) = (isfinite(x.lo) & isfinite(x.hi))
+
+
