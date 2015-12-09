8d407f7dc2caf53123fffa0cede968bd6d33c2de
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-09 12:35:05 -0500

    Update compare.jl

--- src/type/compare.jl
+++ src/type/compare.jl
@@ -1,9 +1,17 @@
+# refs:
+#     "Definition of the Arithmetic Operations and Comparison Relations
+#        for an Interval Arithmetic Standard" by Gerd Bohlender and Ulrich Kulisch
+#     Reliable Computing 15, 2011 pp 41-42
+#
+#     Computer Arithmetic and Validity (2nd edition) by Ulrich Kulisch (section 7.4, pp242-43)
+
+
 (==){S<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{S,C}) = ((a.lo == b.lo) & (a.hi == b.hi))
 (!=){S<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{S,C}) = ((a.lo != b.lo) | (a.hi != b.hi))
-(< ){S<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{S,C}) = ((a.lo <  b.lo) & (a.hi <  b.hi))
-(> ){S<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{S,C}) = ((a.lo >  b.lo) & (a.hi >  b.hi))
 (<=){S<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{S,C}) = ((a.lo <= b.lo) & (a.hi <= b.hi))
 (>=){S<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{S,C}) = ((a.lo >= b.lo) & (a.hi >= b.hi))
+(< ){S<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{S,C}) = (!(a >= b))
+(> ){S<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{S,C}) = (!(a <= b))
 
 #=
     if a,b are of differing Sculpt and one boundry value from a and one from b are equal 
@@ -13,7 +21,7 @@
 =#
 (==){S<:Sculpt, T<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{T,C}) = false
 (!=){S<:Sculpt, T<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{T,C}) = true
-(<=){S<:Sculpt, T<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{T,C}) = opened(a) <  opened(b)
+(<=){S<:Sculpt, T<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{T,C}) = opened(a) < opened(b)
 (>=){S<:Sculpt, T<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{T,C}) = opened(a) > opened(b)
 (< ){S<:Sculpt, T<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{T,C}) = closed(a) < closed(b)
 (> ){S<:Sculpt, T<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{T,C}) = closed(a) > closed(b)
