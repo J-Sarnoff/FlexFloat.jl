4980c0997c0e57590773699a4fb892fe1a1dd147
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-09 03:25:31 -0500

    Update compare.jl

--- src/type/compare.jl
+++ src/type/compare.jl
@@ -6,7 +6,7 @@
 (>=){S<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{S,C}) = ((a.lo >= b.lo) & (a.hi >= b.hi))
 
 #=
-    if a,b are of differing Sculpt and one boundry value of a,b are equal 
+    if a,b are of differing Sculpt and one boundry value from a and one from b are equal 
     then (a <= b or a >= b) and ( !(a<b) and !(a>b) and !(a==b) )
 =#
 (==){S<:Sculpt, T<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{T,C}) = false
