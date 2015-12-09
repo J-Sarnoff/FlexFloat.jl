3a29fd4fb54a8bf39cc803b36f5c98a173f4ccb2
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-09 01:27:44 -0500

    Update FlexFloat.jl

--- FlexFloat.jl
+++ FlexFloat.jl
@@ -1,5 +1,12 @@
 module FlexFloat
 
+# provide these modular services
+
+include("module/AdjacentFloat/AdjacentFloat.jl")
+using .AdjacentFloat
+include("module/RoundFast/RoundFast.jl")
+using .RoundFast
+
 include("type/flexible.jl")
 include("type/primitive.jl")
 
