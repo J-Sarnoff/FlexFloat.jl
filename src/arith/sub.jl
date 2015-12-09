75ea9e19a16cf59f7a5671ba5f47528198d82390
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-09 04:09:05 -0500

    Update sub.jl

--- src/arith/sub.jl
+++ src/arith/sub.jl
@@ -2,3 +2,23 @@
    Flex{negate(S), C}(-a.hi, -a.lo)
 end
 
+for T in (:ClCl, :OpOp)
+   @eval begin
+       function (-){C<:Clay}(a::Flex{$T,C}, b::Flex{$T,C})
+           lo = (-)(a.lo, b.hi, RoundDown)
+           hi = (-)(a.hi, b.lo, RoundUp)
+           Flex{$T,C}(lo,hi)
+       end
+   end       
+end
+
+function (-){S<:Sculpt, T<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{T,C})
+    aLoIsOpen, aHiIsOpen = boundries(S)
+    bLoIsOpen, bHiIsOpen = boundries(T)
+    cType = boundries(aLoIsOpen|bLoIsOpen, aHiIsOpen|bHiIsOpen)
+    
+    lo = (-)(a.lo, b.hi, RoundDown)
+    hi = (-)(a.hi, b.lo, RoundUp)
+       
+    cType(lo,hi)
+end
