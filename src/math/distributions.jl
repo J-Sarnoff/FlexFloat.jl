4886a98771db7e5841569ca2aa460896e1fde53e
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-09 13:26:42 -0500

    Update distributions.jl

--- src/math/distributions.jl
+++ src/math/distributions.jl
@@ -1,14 +1,12 @@
    import Main.Distributions:cdf,pdf,quantile
    typealias Dist Main.Distributions.Distribution{Main.Distributions.Univariate,Main.Distributions.Continuous}
-   typealias AbstractMixtureDist Main.Distributions.AbstractMixtureModel{Main.Distributions.Univariate, Main.Distributions.Continuous, Main.Distributions.Distribution}
+   typealias AbstractMixtureDist Main.Distributions.AbstractMixtureModel{Main.Distributions.Univariate, Main.Distributions.Continuous,C<: Main.Distributions.Distribution}
    
    # quash ambiguity
    cdf{S<:Sculpt, C<:Clay}(d::Main.Distributions.Triweight, x::Flex{S,C}) = ErrorException("Not Implemented")
    pdf{S<:Sculpt, C<:Clay}(d::Main.Distributions.Triweight, x::Flex{S,C}) = ErrorException("Not Implemented")
    pdf{S<:Sculpt, C<:Clay}(d::AbstractMixtureDist, x::Flex{S,C}) = ErrorException("Not Implemented")
    
- pdf(Distributions.AbstractMixtureModel{Distributions.Univariate, Distributions.Continuous, C<:Distributions.Distribution}, FlexFloat.Flex{#S<:FlexFloat.Sculpt, #C<:AbstractFloat})
-
 
    @inline negabs(x::Clay) = -abs(x)
 
