5b7332bb8041286bc068f58a4dfc4c3565567cfc
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-09 02:21:27 -0500

    Update primitive.jl

--- src/type/primitive.jl
+++ src/type/primitive.jl
@@ -23,13 +23,13 @@ boundries{B<:Bool}(lo::B,hi::B) = Boundries[ one(Int8)+reinterpret(Int8,lo)+(rei
     Closed bounds are reached but Not Exceeded.
     Open bounds are approached but Not Reached.
     
-    cover(Flex)   gives the ClCl inclusion of Flex
-    uncover(Flex) gives the OpOp inclusion of Flex
+    close(Flex)   gives the ClCl inclusion of Flex
+    open(Flex) gives the OpOp inclusion of Flex
 =#
 
-cover{S<:Sculpt,C<:Clay}(x::Flex{S,C}) = ClCl(x.lo, x.hi)
+close{S<:Sculpt,C<:Clay}(x::Flex{S,C}) = ClCl(x.lo, x.hi)
 
-uncover{C<:Clay}(x::Flex{ClCl,C}) = OpOp(prevFloat(x.lo), nextFloat(x.hi))
-uncover{C<:Clay}(x::Flex{ClOp,C}) = OpOp(prevFloat(x.lo), x.hi)
-uncover{C<:Clay}(x::Flex{OpCl,C}) = OpOp(x.lo, nextFloat(x.hi))
-uncover{C<:Clay}(x::Flex{OpOp,C}) = x
+open{C<:Clay}(x::Flex{ClCl,C}) = OpOp(prevFloat(x.lo), nextFloat(x.hi))
+open{C<:Clay}(x::Flex{ClOp,C}) = OpOp(prevFloat(x.lo), x.hi)
+open{C<:Clay}(x::Flex{OpCl,C}) = OpOp(x.lo, nextFloat(x.hi))
+open{C<:Clay}(x::Flex{OpOp,C}) = x
