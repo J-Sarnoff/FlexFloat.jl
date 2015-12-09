8aeb7f0d96e201ecc41c0f1c3188b9ea96b538ad
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-09 02:15:59 -0500

    Update eftRound.jl

--- module/RoundFast/eftRound.jl
+++ module/RoundFast/eftRound.jl
@@ -1,45 +1,4 @@
 
-#=
-       RoundDown
-       hi  lo  rounding        fastrounding
-       --------------------------------------------
-       +   +   hi              hi
-       +   -   prevfloat(hi)   nextNearerToZero(hi) == prevFloat(hi)
-       -   +   hi              hi
-       -   -   prevfloat(hi)   nextAwayFromZero(hi) == prevFloat(hi)
-       RoundUp
-       hi  lo  rounding        fastrounding
-       --------------------------------------------
-       +   +   nextfloat(hi)   nextAwayFromZero(hi) == nextFloat(hi)
-       +   -   hi              hi
-       -   +   nextfloat(hi)   nextNearerToZero(hi) == nextFloat(hi)
-       -   -   hi              hi
-       RoundFromZero
-       hi  lo  rounding        fastrounding
-       --------------------------------------------
-       +   +   nextfloat(hi)   nextAwayFromZero(hi)
-       +   -   hi              hi
-       -   +   hi              hi
-       -   -   prevfloat(hi)   nextAwayFromZero(hi
-       RoundToZero
-       hi  lo  rounding        fastrounding
-       --------------------------------------------
-       +   +   hi              hi
-       +   -   prevfloat(hi)   nextNearerToZero(hi)
-       -   +   nextfloat(hi)   nextNearerToZero(hi)
-       -   -   hi              hi
-      RoundNearest
-       hi  lo  rounding        fastrounding
-       --------------------------------------------
-       +   +   hi              hi
-       +   -   hi              hi
-       -   +   hi              hi
-       -   -   hi              hi
-=#
-
-
-
-
 @inline eftRound{T<:AbstractFloat}(hi::T, lo::T, ::RoundingMode{:ToZero}) = 
      (((signbit(hi)==signbit(lo)) | (lo==zero(T))) ? hi : nextNearerToZero(hi))
 
