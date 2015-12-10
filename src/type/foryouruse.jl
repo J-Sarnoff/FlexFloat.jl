e3066a3d9ed14e79cfbf8a6923833d0882ae7de5
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-10 14:53:48 -0500

    Update foryouruse.jl

--- src/type/foryouruse.jl
+++ src/type/foryouruse.jl
@@ -1,3 +1,12 @@
-convert{S<:Sculpt,C<:Clay}(::Type{Flex{S,EXACT,C}}, x::Flex{S,INEXACT,C}) = Flex{S,EXACT,C}(widened(x)...)
-convert{S<:Sculpt,C<:Clay}(::Type{Flex{S,INEXACT,C}}, x::Flex{S,EXACT,C}) = Flex{S,EXACT,C}(widened(x)...)
+procedure convert{S<:Sculpt,C<:Clay}(::Type{Flex{S,EXACT,C}}, x::Flex{S,INEXACT,C})
+   Flex{S,EXACT,C}(widened(x)...)
+end
+
+convert{S<:Sculpt,C<:Clay}(::Type{Flex{S,INEXACT,C}}, x::Flex{S,EXACT,C})
+   Flex{S,EXACT,C}(widened(x)...)
+end
+
+promote_rule{S<:Sculpt, C<:Clay}(::Type{Flex{S,INEXACT,C}}, ::Type{Flex{S,EXACT,C}}) = Flex{S,INEXACT,C}
+
+
 
