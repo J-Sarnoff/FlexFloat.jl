0d41c56b1f6f8272cb9ac8ba1295bafea501e0f2
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-10 17:21:37 -0500

    Update foryouruse.jl

--- src/type/foryouruse.jl
+++ src/type/foryouruse.jl
@@ -1,8 +1,8 @@
-procedure convert{S<:Sculpt,C<:Clay}(::Type{Flex{S,EXACT,C}}, x::Flex{S,INEXACT,C})
+function convert{S<:Sculpt,C<:Clay}(::Type{Flex{S,EXACT,C}}, x::Flex{S,INEXACT,C})
    Flex{S,EXACT,C}(widened(x)...)
 end
 
-convert{S<:Sculpt,C<:Clay}(::Type{Flex{S,INEXACT,C}}, x::Flex{S,EXACT,C})
+function convert{S<:Sculpt,C<:Clay}(::Type{Flex{S,INEXACT,C}}, x::Flex{S,EXACT,C})
    Flex{S,EXACT,C}(widened(x)...)
 end
 
