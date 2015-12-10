8f156324088236371da9e6b986b132fd8b2cd592
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-10 14:57:41 -0500

    Update compare.jl

--- src/type/compare.jl
+++ src/type/compare.jl
@@ -6,23 +6,23 @@
 #     Computer Arithmetic and Validity (2nd edition) by Ulrich Kulisch (section 7.4, pp242-43)
 
 
-(==){S<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{S,C}) = ((a.lo == b.lo) & (a.hi == b.hi))
-(!=){S<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{S,C}) = ((a.lo != b.lo) | (a.hi != b.hi))
-(<=){S<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{S,C}) = ((a.lo <= b.lo) & (a.hi <= b.hi))
-(>=){S<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{S,C}) = ((a.lo >= b.lo) & (a.hi >= b.hi))
-(< ){S<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{S,C}) = (!(a >= b))
-(> ){S<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{S,C}) = (!(a <= b))
+(==){S<:Sculpt, Q<:Qualia, C<:Clay}(a::Flex{S,Q,C}, b::Flex{S,Q,C}) = ((a.lo == b.lo) & (a.hi == b.hi))
+(!=){S<:Sculpt, Q<:Qualia, C<:Clay}(a::Flex{S,Q,C}, b::Flex{S,Q,C}) = ((a.lo != b.lo) | (a.hi != b.hi))
+(<=){S<:Sculpt, Q<:Qualia, C<:Clay}(a::Flex{S,Q,C}, b::Flex{S,Q,C}) = ((a.lo <= b.lo) & (a.hi <= b.hi))
+(>=){S<:Sculpt, Q<:Qualia, C<:Clay}(a::Flex{S,Q,C}, b::Flex{S,Q,C}) = ((a.lo >= b.lo) & (a.hi >= b.hi))
+(< ){S<:Sculpt, Q<:Qualia, C<:Clay}(a::Flex{S,Q,C}, b::Flex{S,Q,C}) = (!(a >= b))
+(> ){S<:Sculpt, Q<:Qualia, C<:Clay}(a::Flex{S,Q,C}, b::Flex{S,Q,C}) = (!(a <= b))
 
 #=
-    if a,b are of differing Sculpt and one boundry value from a and one from b are equal 
+    if a,b are of differing Sculpt and one boundry value from each of a and b are equal 
     then (a <= b or a >= b) and ( !(a<b) and !(a>b) and !(a==b) )
     
      ⟨1.0, 2.0⟫ <=   ⟨1.0, 2.0⟩ <= ⟪1.0, 2.0 ⟩ ,  ⟪1.0, 2.0⟫
 =#
-(==){S<:Sculpt, T<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{T,C}) = false
-(!=){S<:Sculpt, T<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{T,C}) = true
-(<=){S<:Sculpt, T<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{T,C}) = opened(a) < opened(b)
-(>=){S<:Sculpt, T<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{T,C}) = opened(a) > opened(b)
-(< ){S<:Sculpt, T<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{T,C}) = closed(a) < closed(b)
-(> ){S<:Sculpt, T<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{T,C}) = closed(a) > closed(b)
+(==){S<:Sculpt, T<:Sculpt, Q<:Qualia, C<:Clay}(a::Flex{S,Q,C}, b::Flex{T,Q,C}) = false
+(!=){S<:Sculpt, T<:Sculpt, Q<:Qualia, C<:Clay}(a::Flex{S,Q,C}, b::Flex{T,Q,C}) = true
+(<=){S<:Sculpt, T<:Sculpt, Q<:Qualia, C<:Clay}(a::Flex{S,Q,C}, b::Flex{T,Q,C}) = opened(a) < opened(b)
+(>=){S<:Sculpt, T<:Sculpt, Q<:Qualia, C<:Clay}(a::Flex{S,Q,C}, b::Flex{T,Q,C}) = opened(a) > opened(b)
+(< ){S<:Sculpt, T<:Sculpt, Q<:Qualia, C<:Clay}(a::Flex{S,Q,C}, b::Flex{T,Q,C}) = closed(a) < closed(b)
+(> ){S<:Sculpt, T<:Sculpt, Q<:Qualia, C<:Clay}(a::Flex{S,Q,C}, b::Flex{T,Q,C}) = closed(a) > closed(b)
 
