6774fb289f82896f70b9de13a11dc202c77a2c44
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-10 16:59:34 -0500

    Update add.jl

--- src/arith/add.jl
+++ src/arith/add.jl
@@ -1,14 +1,14 @@
 for T in (:CLCL, :OPOP)
    @eval begin
-       function (+){C<:Clay}(a::Flex{$T,C}, b::Flex{$T,C})
+       function (+){Q<:Qualia,C<:Clay}(a::Flex{$T,Q,C}, b::Flex{$T,Q,C})
            lo = (+)(a.lo, b.lo, RoundDown)
            hi = (+)(a.hi, b.hi, RoundUp)
-           Flex{$T,C}(lo,hi)
+           Flex{$T,Q,C}(lo,hi)
        end
    end       
 end
 
-function (+){S<:Sculpt, T<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{T,C})
+function (+){S<:Sculpt, T<:Sculpt, Q<:Qualia, C<:Clay}(a::Flex{S,Q,C}, b::Flex{T,Q,C})
     aLoIsOpen, aHiIsOpen = boundries(S)
     bLoIsOpen, bHiIsOpen = boundries(T)
     sculpting = boundries(aLoIsOpen|bLoIsOpen, aHiIsOpen|bHiIsOpen)
@@ -16,11 +16,11 @@ function (+){S<:Sculpt, T<:Sculpt, C<:Clay}(a::Flex{S,C}, b::Flex{T,C})
     lo = (+)(a.lo, b.lo, RoundDown)
     hi = (+)(a.hi, b.hi, RoundUp)
        
-    Flex{sculpting, C}(lo,hi)
+    Flex{sculpting, Q, C}(lo,hi)
 end
 
-(+){S<:Sculpt,C<:Clay}(a::Flex{S,C}, b::C) = (+)(a, Flex{S,C}(b))
-(+){S<:Sculpt,C<:Clay}(a::C, b::Flex{S,C}) = (+)(Flex{S,C}(a), b)
+(+){S<:Sculpt,Q<:Qualia,C<:Clay}(a::Flex{S,Q,C}, b::C) = (+)(a, Flex{S,Q,C}(b))
+(+){S<:Sculpt,Q<:Qualia,C<:Clay}(a::C, b::Flex{S,Q,C}) = (+)(Flex{S,Q,C}(a), b)
 
-(+){S<:Sculpt,C<:Clay,T<:Real}(a::Flex{S,C}, b::T) = (+)(a, Flex{S,C}(convert(C,b)))
-(+){S<:Sculpt,C<:Clay,T<:Real}(a::T, b::Flex{S,C}) = (+)(Flex{S,C}(convert(C,a)), b)
+(+){S<:Sculpt,Q<:Qualia,C<:Clay,T<:Real}(a::Flex{S,Q,C}, b::T) = (+)(a, Flex{S,Q,C}(convert(C,b)))
+(+){S<:Sculpt,Q<:Qualia,C<:Clay,T<:Real}(a::T, b::Flex{S,Q,C}) = (+)(Flex{S,Q,C}(convert(C,a)), b)
