c1c269948c6a22c7da3c8cfa4ad16293ab75cc01
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-10 12:48:56 -0500

    Update io.jl

--- src/type/io.jl
+++ src/type/io.jl
@@ -1,34 +1,35 @@
-const Approx = "~"
-const LowRing = "˳"
-const Flexable = "⍿"
+const Exactly   = "⌁"
+const Inexactly = "~"
+const Simply    = "˳"
+const Flexably = "⍿"
 const OpenAbove = "⫯"
 const OpenBelow = "⫰"
 
 # select characters corresponding to boundries as specified (lo,hi)
 #   true for Open, false for Closed
 #                     ClCl      ClOp        OpCl      OpOp
-#const Delimiters = [ ("⟨","⟩"), ("⟨","⟫"), ("⟪","⟩"), ("⟪","⟫") ];
+const Delimiters = [ ("⟨","⟩"), ("⟨","⟫"), ("⟪","⟩"), ("⟪","⟫") ];
 
 #                     ClCl      ClOp        OpCl      OpOp
-const Delimiters = [ (Flexable, Flexable), (Flexable, OpenAbove), (OpenBelow, Flexable), (OpenBelow,OpenAbove) ];
+#const Delimiters = [ (Flexable, Flexable), (Flexable, OpenAbove), (OpenBelow, Flexable), (OpenBelow,OpenAbove) ];
 
 @inline delimiters(loIsOpen::Bool, hiIsOpen::Bool) =
       Delimiters[ one(Int8)+(reinterpret(Int8,loIsOpen)<<1)+hiIsOpen ]
 
 function show{S<:Sculpt, Q<:Qualia, C<:Clay}(io::IO, x::Flex{S,Q,C})
-    prefix = (Q==INEXACT) ? Approx : ""
+    tiesym = (Q==INEXACT) ? Inexactly : Exactly
     delimLo, delimHi = delimiters(boundries(S)...)
     # s = (x.lo != x.hi) ? string(x.lo, ", ", x.hi) : string(x.lo)
-    s = (x.lo != x.hi) ? string(x.lo, LowRing, x.hi) : string(x.lo)
+    s = (x.lo != x.hi) ? string(x.lo, tiesym, x.hi) : string(x.lo)
     s = string(prefix, delimLo, s, delimHi)
     print(io, s)
 end
 
 function showcompact{S<:Sculpt, Q<:Qualia, C<:Clay}(io::IO, x::Flex{S,Q,C})
-    prefix = (Q==INEXACT) ? Approx : ""
+    tiesym = (Q==INEXACT) ? Inexactly : Exactly
     delimLo, delimHi = delimiters(boundries(S)...)
-    lo = @sprintf("%0.4g", x.lo)
-    hi = @sprintf("%0.5g", x.hi)
+    lo = @sprintf("%6.5g", x.lo)
+    hi = @sprintf("%6.5g", x.hi)
     # s = (x.lo != x.hi) ? lo : string(lo, ", ", hi)
     s = (x.lo != x.hi) ? lo : string(lo, FlexGlyph, hi)
     s = string(prefix, delimLo, s, delimHi)
