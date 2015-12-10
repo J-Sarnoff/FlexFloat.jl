2d9bfbb27072dfc6c88cae21155aabe840b5541f
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-09 21:34:49 -0500

    Update protenaxis.jl

--- src/behavior/protenaxis.jl
+++ src/behavior/protenaxis.jl
@@ -2,7 +2,7 @@
 
   nomenclature
 
-    An e-bit enfolding floating point value is an *augmented* float.
+    An e-bit enfolding floating point value is an 'augmented' float.
     An augmented float that has its e-bit clear (0b0) is 'situated'.
     An augmented float that has its e-bit set   (0b1) is 'enhanced'.
 
@@ -11,6 +11,7 @@
 # query state
 enhanced{T<:AbstractFloat}(x::T) =  tst_ebit(x)  #      is this enhanced?
 situated{T<:AbstractFloat}(x::T) = !tst_ebit(x)  #      is this situated?
+
 # force state
  enhance!{T<:AbstractFloat}(x::T) =  set_ebit(x)  # this is now  enhanced!
  situate!{T<:AbstractFloat}(x::T) =  clr_ebit(x)  # this is now  situated!
