714de340dc3561f3ad0008a81992a9a9f933b5ee
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-10 16:41:49 -0500

    Update convert.jl

--- src/type/convert.jl
+++ src/type/convert.jl
@@ -19,28 +19,6 @@ function convert{S<:Sculpt,Q<:Qualia,C<:Clay}(::Type{Array{C,1}}, x::Flex{S,Q,C}
    (lo,hi)
 end
 
-function domainExtendedLogarithmicMean{R<:Real}(x::R,y::R)
-       lo,hi = minmax(x,y)
-       if lo > 0.0
-           #=
-               using the logarithmic mean to condense the spanned interval into a representative floating point value
-           =#
-           (hi - lo) / (log(hi) - log(lo))
-        else # determine the logarithmic mean indirectly
-           if signbit(hi) == signbit(lo)
-                abs(hi - lo) / (log(abs(hi)) - log(abs(lo)))  
-           else
-              lo = abs(hi) / log(abs(lo))
-              hi = abs(lo) / log(abs(hi))
-              if abs(lo) <= abs(hi)
-                 copysign( abs(lo-hi), lo)
-              else
-                 copysign( abs(lo-hi), hi)
-              end
-            end
-        end    
-end
-
 function convert{S<:Sculpt,Q<:Qualia,C<:Clay,R<:Real}(::Type{R}, x::Flex{S,Q,C})
    lo,hi = (Q==CLCL) ? values(x) : values(opened(x))
    lo = convert(R,lo)
