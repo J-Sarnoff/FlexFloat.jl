45975c7a7b6d8e0c5030eaa31a238b3bd2a5043a
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-10 12:53:57 -0500

    Update io.jl

--- src/type/io.jl
+++ src/type/io.jl
@@ -19,7 +19,6 @@ const Delimiters = [ ("⟨","⟩"), ("⟨","⟫"), ("⟪","⟩"), ("⟪","⟫") 
 function show{S<:Sculpt, Q<:Qualia, C<:Clay}(io::IO, x::Flex{S,Q,C})
     tiesym = (Q==INEXACT) ? Inexactly : Exactly
     delimLo, delimHi = delimiters(boundries(S)...)
-    # s = (x.lo != x.hi) ? string(x.lo, ", ", x.hi) : string(x.lo)
     s = (x.lo != x.hi) ? string(x.lo, tiesym, x.hi) : string(x.lo)
     s = string(delimLo, s, delimHi)
     print(io, s)
@@ -28,10 +27,9 @@ end
 function showcompact{S<:Sculpt, Q<:Qualia, C<:Clay}(io::IO, x::Flex{S,Q,C})
     tiesym = (Q==INEXACT) ? Inexactly : Exactly
     delimLo, delimHi = delimiters(boundries(S)...)
-    lo = @sprintf("%6.5g", x.lo)
-    hi = @sprintf("%6.5g", x.hi)
-    # s = (x.lo != x.hi) ? lo : string(lo, ", ", hi)
-    s = (x.lo != x.hi) ? lo : string(lo, FlexGlyph, hi)
+    lo = @sprintf("%0.5g", x.lo)
+    hi = @sprintf("%0.5g", x.hi)
+    s = (x.lo != x.hi) ? string(lo, FlexGlyph, hi) : lo
     s = string(delimLo, s, delimHi)
     print(io, s)
 end
