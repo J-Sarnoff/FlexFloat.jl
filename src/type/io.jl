fca3f9b2e3cd0585e23d4a8ef84e35517c51e77a
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-10 14:04:38 -0500

    Update io.jl

--- src/type/io.jl
+++ src/type/io.jl
@@ -18,18 +18,19 @@ const Delimiters = [ ("▪","▪"), ("▪","▫"), ("▫","▪"), ("▫","▫") 
       Delimiters[ one(Int8)+(reinterpret(Int8,loIsOpen)<<1)+hiIsOpen ]
 
 # when lo==hi, to indicate EXACT
-const Surrounds = [("⬩","⬩"), ("⬩","⬦"), ("⬦","⬩"),("⬩","⬦")];
+const Surrounds = [("⬩","⬩"), ("⬩","⬦"), ("⬦","⬩"),("⬦","⬦")];
 @inline surrounds(loIsOpen::Bool, hiIsOpen::Bool) =
       Surrounds[ one(Int8)+(reinterpret(Int8,loIsOpen)<<1)+hiIsOpen ]
 
 function show{S<:Sculpt, Q<:Qualia, C<:Clay}(io::IO, x::Flex{S,Q,C})
     delimLo, delimHi = delimiters(boundries(S)...)
+    aroundLo, aroundHi = delimLo, delimHi
     if (Q==INEXACT)
        tiesym = Inexactly
-       aroundLo, aroundHi = delimLo, delimHi
+       #aroundLo, aroundHi = delimLo, delimHi
     else
        tiesym = Exactly
-       aroundLo, aroundHi = surrounds(boundries(S)...)
+       #aroundLo, aroundHi = surrounds(boundries(S)...)
     end       
     s = (x.lo != x.hi) ? string(delimLo, x.lo, tiesym, x.hi, delimHi) : string(aroundLo,x.lo, aroundHi)
     print(io, s)
@@ -37,12 +38,13 @@ end
 
 function showcompact{S<:Sculpt, Q<:Qualia, C<:Clay}(io::IO, x::Flex{S,Q,C})
     delimLo, delimHi = delimiters(boundries(S)...)
+    aroundLo, aroundHi = delimLo, delimHi
     if (Q==INEXACT)
        tiesym = Inexactly
-       aroundLo, aroundHi = delimLo, delimHi
+       #aroundLo, aroundHi = delimLo, delimHi
     else
        tiesym = Exactly
-       aroundLo, aroundHi = surrounds(boundries(S)...)
+       #aroundLo, aroundHi = surrounds(boundries(S)...)
     end       
     lo = @sprintf("%0.5g", x.lo)
     hi = @sprintf("%0.5g", x.hi)
