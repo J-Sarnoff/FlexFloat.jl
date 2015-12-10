20fcbb9df31fa94d87cc710db80c1dd7a025f17f
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-10 16:41:14 -0500

    Update valueops.jl

--- src/arith/valueops.jl
+++ src/arith/valueops.jl
@@ -1,20 +1,20 @@
-signbit{S<:Sculpt,C<:Clay}(a::Flex{S,C}) = (signbit(a.lo),signbit(a.hi))
-sign{S<:Sculpt,C<:Clay}(a::Flex{S,C}) = (sign(a.lo),sign(a.hi))
+signbit{S<:Sculpt,Q<:Qualia,C<:Clay}(a::Flex{S,Q,C}) = (signbit(a.lo),signbit(a.hi))
+sign{S<:Sculpt,Q<:Qualia,C<:Clay}(a::Flex{S,Q,C}) = (sign(a.lo),sign(a.hi))
 
-function copysign{S<:Sculpt,C<:Clay}(a::Flex{S,C}, b::Real)
+function copysign{S<:Sculpt,Q<:Qualia,C<:Clay}(a::Flex{S,Q,C}, b::Real)
     lo = copysign(a.lo,b)
     hi = copysign(a.hi,b)
     lo <= hi ? (S)(lo,hi) : (negate(S))(hi,lo)
 end
 
-function flipsign{S<:Sculpt,C<:Clay}(a::Flex{S,C}, b::Real)
+function flipsign{S<:Sculpt,Q<:Qualia,C<:Clay}(a::Flex{S,Q,C}, b::Real)
     lo = flipsign(a.lo,b)
     hi = flipsign(a.hi,b)
     lo <= hi ? (S)(lo,hi) : (negate(S))(hi,lo)
 end
 
-frexp{S<:Sculpt,C<:Clay}(a::Flex{S,C}) = (frexp(a.lo),frexp(a.hi))
-ldexp{S<:Sculpt,C<:Clay,I<:Union{Int32,Int64}}(::Type{S},a::Tuple{Tuple{C,I},Tuple{C,I}}) = 
+frexp{S<:Sculpt,Q<:Qualia,C<:Clay}(a::Flex{S,C}) = (frexp(a.lo),frexp(a.hi))
+ldexp{S<:Sculpt,Q<:Qualia,C<:Clay,I<:Union{Int32,Int64}}(::Type{S},a::Tuple{Tuple{C,I},Tuple{C,I}}) = 
    (S)(ldexp(a[1][1],a[1][2]),ldexp(a[2][1],a[2][2]))
 ldexp{C<:Clay,I<:Union{Int32,Int64}}(a::Tuple{Tuple{C,I},Tuple{C,I}}) = 
    (ClCl)(ldexp(a[1][1],a[1][2]),ldexp(a[2][1],a[2][2]))
@@ -37,8 +37,9 @@ Special values:
 
 #decompose{S<:Sculpt, C<:Clay}(x::Flex{S,C}) = decompose( mid(x) )
 
-isnan{S<:Sculpt, C<:Clay}(x::Flex{S,C}) = (isnan(x.lo) | isnan(x.hi))
-isinf{S<:Sculpt, C<:Clay}(x::Flex{S,C}) = (isinf(x.lo) | isinf(x.hi))
-isfinite{S<:Sculpt, C<:Clay}(x::Flex{S,C}) = (isfinite(x.lo) & isfinite(x.hi))
+isnan{S<:Sculpt, Q<:Qualia, C<:Clay}(x::Flex{S,Q,C}) = (isnan(x.lo) | isnan(x.hi))
+isinf{S<:Sculpt, Q<:Qualia, C<:Clay}(x::Flex{S,Q,C}) = (isinf(x.lo) | isinf(x.hi))
+isfinite{S<:Sculpt, Q<:Qualia, C<:Clay}(x::Flex{S,Q,C}) = (isfinite(x.lo) & isfinite(x.hi))
+issubnormal{S<:Sculpt, Q<:Qualia, C<:Clay}(x::Flex{S,Q,C}) = (issubnormal(x.lo) | issubnormal(x.hi))
 
 
