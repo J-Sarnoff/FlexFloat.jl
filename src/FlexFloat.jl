40fa2c9abec334431de51db119e472eaacf3a439
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-10 22:50:07 -0500

    Update FlexFloat.jl

--- src/FlexFloat.jl
+++ src/FlexFloat.jl
@@ -49,7 +49,7 @@ include("type/primitive.jl")
 include("type/convert.jl")
 include("type/interval.jl")
 include("type/compare.jl")
-include("type/condense.jl")
+include("type/distill.jl")
 include("type/foryouruse.jl")
 include("type/io.jl")
 
