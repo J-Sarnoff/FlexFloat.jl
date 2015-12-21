3fc265cf17c53289a8c8c89060435eb8d89c8f7a
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2015-12-21 04:57:49 -0500

    Update _newtype.jl

--- src/type/_newtype.jl
+++ src/type/_newtype.jl
@@ -12,12 +12,24 @@ type Exact  <: ExactState  end; ExactBound  = Exact()
 type Closed <: ClosedState end; ClosedBound = Closed()
 type Open   <: OpenState   end; OpenBound   = Open()
 
-type FlexibleInterval{L<:BoundingState,H<:BoundingState,F<:AbstractFloat} <: Real
-    lo::F
-    hi::F
-    blo::L
-    bhi::H
+
+abstract RealInterval     <: Real
+
+abstract AbstractBoundary
+abstract BoundingState    <: AbstractBoundary
+abstract ExactState          <: BoundingState
+abstract ClosedState         <: BoundingState
+abstract OpenState           <: BoundingState
+
+# singleton types
+type Exact  <: ExactState  end; ExactBound  = Exact()
+type Closed <: ClosedState end; ClosedBound = Closed()
+type Open   <: OpenState   end; OpenBound   = Open()
+
+type FlexibleInterval{LoState,HiState} <: Real
+    lo::Real
+    hi::Real
 end
 
-# fi = FlexibleInterval( 0.0f0, 1.0f0, ExactBound, ClosedBound )
-
+# fi = FlexibleIterval{LoState,HiState}(1.0,2.0)
+# FlexibleInterval{Exact,Closed}(1.0, 2.0)
